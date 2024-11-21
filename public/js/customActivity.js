a2fb22ca963b33a8acf246ee7359af118552c2be
annamuniz95 <anna.muniz@stepahead.pt>
2024-11-21 12:19:44 +0000

    alteracao para buscar valor de checkbox

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -67,20 +67,20 @@ define([
     }
 
     function save() {
-        var nomeInput = $('#input_nome').val();
+        /*var nomeInput = $('#input_nome').val();
         var apelidoInput = $('#input_apelido').val();
-        var idadeInput = $('#input_idade').val();
+        var idadeInput = $('#input_idade').val();*/
 
-        var opcao_1 = $('#opcao_1').val();
-        var opcao_2 = $('#opcao_2').val();
-        var opcao_3 = $('#opcao_3').val();
+        var opcao_1 = $('#opcao_1').is(':checked');
+        var opcao_2 = $('#opcao_2').is(':checked');
+        var opcao_3 = $('#opcao_3').is(':checked');
 
         payload['arguments'].execute.inArguments = [{
             "tokens": authTokens,
             "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}",
-            "nome":nomeInput,
-            "apelido":apelidoInput,
-            "idade": idadeInput,
+            //"nome":nomeInput,
+            //"apelido":apelidoInput,
+            //"idade": idadeInput,
             "opcao1": opcao_1,
             "opcao2": opcao_2,
             "opcao3": opcao_3
