6f77f808430d6a8d6f01e345429552caa2e6a434
annamuniz95 <anna.muniz@stepahead.pt>
2024-12-03 15:36:12 +0000

    alteracao require

--- public/js/getPushInformation.js
+++ public/js/getPushInformation.js
@@ -4,8 +4,8 @@
 
 function retornarMensagem(){
     alert("voce clicou aqui");
-    console.log("dentro da função retornar mensagem");
-    console.log(window.getRequestedInteractionBody());
+    /*console.log("dentro da função retornar mensagem");
+    console.log(window.getRequestedInteractionBody());*/
     //pegar aqui info do nome da jornada e passar para o endpoint
 
 }
